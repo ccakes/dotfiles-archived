bd7840aeb528ff37c3d3442dc225c10bf1c3b85a
Cameron Daniel <cam.daniel@gmail.com>
2017-04-24 11:57:57 +1000

    fix ln fail

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,7 +7,7 @@ echo "(Copies saves in $PWD/backup/)"
 for file in ${DFILES[@]}; do
   if [ -f $HOME/$file ]; then
     cp $HOME/$file $PWD/backup/$file
-    rm -i $HOME/$file 2>/dev/null
+    rm -i $HOME/$file
   fi
 done
 
@@ -18,10 +18,10 @@ curl -sL -o $HOME/.z.sh https://raw.githubusercontent.com/rupa/z/master/z.sh
 curl -sL -o $HOME/.antigen.zsh git.io/antigen
 
 echo "Copying dotfiles into place"
-ln -s $HOME/.gitconfig $PWD/gitconfig
-ln -s $HOME/.perltidyrc $PWD/perltidyrc
-ln -s $HOME/.tmux.conf $PWD/tmux.conf
-ln -s $HOME/.zprofile $PWD/zprofile
+ln -s $PWD/gitconfig $HOME/.gitconfig
+ln -s $PWD/perltidyrc $HOME/.perltidyrc
+ln -s $PWD/tmux.conf $HOME/.tmux.conf
+ln -s $PWD/zprofile $HOME/.zprofile
 
 echo ""
 echo "Finished!"
